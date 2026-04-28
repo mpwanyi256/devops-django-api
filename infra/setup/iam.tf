41f8cc5677624ed280722d6e9ae48498ad3591ad
Samuel Mpwanyi <samuelmpwanyi@gmail.com>
2026-04-28 12:37:10 +0300

    feat(core): Added IAM user for CICD

---
+++ infra/setup/iam.tf
@@ -0,0 +1,60 @@
+################################################
+# Create IAM user and policies for CICD account #
+################################################
+
+resource "aws_iam_user" "cd" {
+  name = "recipe-app-api-cd"
+}
+
+resource "aws_iam_access_key" "cd" {
+  user = aws_iam_user.cd.name
+}
+
+################################################
+# Policies for S3 and Dynamo DB access #
+# This is an outline for permissions that we can use to create an iam policy with aws_iam_policy
+################################################
+
+data "aws_iam_policy_document" "tf_backend" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
+  }
+
+  # The tf-state-deploy is the bucket in the deploy/main.tf file
+  # where we are saving the Terraform state
+  # We are not adding policies for setup because that is going to be run locally in development
+  statement {
+    effect  = "Allow"
+    actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+    resources = [
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = ["arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"]
+  }
+}
+
+# Create a new iam policy based on the permissions above
+resource "aws_iam_policy" "tf_backend" {
+  name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
+  description = "Allow user to use S3 & Dynamo DB for TF BE resources"
+  policy      = data.aws_iam_policy_document.tf_backend.json
+}
+
+# Attach/Assign the policy
+resource "aws_iam_user_policy_attachment" "tf_backend" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.tf_backend.arn
+}
